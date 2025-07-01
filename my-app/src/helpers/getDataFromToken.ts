8bf66574bd380fde07b2328030c0ec1037491588
xaanyaax <aanyasingh282004@gmail.com>
2025-07-02 01:24:10 +0530

    commir

---
+++ my-app/src/helpers/getDataFromToken.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const encodedToken = request.cookies.get("token")?.value || "";
+        const decodedToken:any = jwt.verify(
+            encodedToken,
+            process.env.JWT_SECRET_KEY!
+        );
+        return decodedToken.id;
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+};
