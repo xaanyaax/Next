cb3683cd892b8eaa200b24ee4c12dfe08195d488
xaanyaax <aanyasingh282004@gmail.com>
2025-07-01 21:12:50 +0530

    commit

--- my-app/src/app/profile/page.tsx
+++ my-app/src/app/profile/page.tsx
@@ -1,38 +1,37 @@
 "use client"
 import axios from "axios"
-import { log } from "console"
 import Link from "next/link"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
-export default function Profilepage(){
+export default function Profilepage() {
 
     const router = useRouter();
 
-const logout = async () => {
-    try {
+    const logout = async () => {
+        try {
 
-        const response = await axios.get("/api/users/logout")
-        
-        console.log("Logout successful")
-        router.push("/login")
+            const response = await axios.get("/api/users/logout")
 
-    }catch(error: any) {
-        console.log(error.message)
-        alert("Logout failed: " + error.message);
+            console.log("Logout successful")
+            router.push("/login")
+
+        } catch (error: any) {
+            console.log(error.message)
+            alert("Logout failed: " + error.message);
+
+        }
 
     }
 
-}
-
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">  
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
             <hr />
-            <button 
-            onClick={logout}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+            <button
+                onClick={logout}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button>
         </div>
     )
 }
